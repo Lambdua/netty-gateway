09057bae17da80273d5642c1aa91ef0bd7dd617f
liang tao <liangtao@agile-china.com>
2020-12-24 23:01:16 +0800

    pom依赖，打包构建插件的编写

---
+++ src/main/java/com/lt/sg/SgStater.java
@@ -0,0 +1,12 @@
+package com.lt.sg;
+
+/**
+ * @author liangtao
+ * @description 启动类
+ * @date 2020年12月24 22:53
+ **/
+public class SgStater {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
